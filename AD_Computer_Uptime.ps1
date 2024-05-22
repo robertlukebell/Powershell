5d2bc5271dad7b7bf0a47e71103d5208c80a734f
Luke Bell <robertlukebell@gmail.com>
2024-05-22 18:51:35 -0400

    Add files via upload
    # Script to check AD for all Computers with 7 days or more of uptime and output hostnames in HTML file

---
+++ AD_Computer_Uptime.ps1
@@ -0,0 +1,20 @@
+﻿
+# Script to check AD for all Computers with 7 days or more of uptime and output hostnames in HTML file
+
+$proc = Get-ADComputer -Filter * | ForEach-Object {
+
+    $a = (get-date)-(gcim Win32_OperatingSystem).LastBootUpTime
+    $d = $a.Days.tostring()
+
+    If ($a.days -ge 7)
+        {
+            [PSCustomObject]@{
+            'Host Name' = $_.name 
+            'Uptime' = $d + ' days'
+            }
+        }
+    }
+
+$proc | ConvertTo-Html -Property "Host Name","Uptime" | Out-File -FilePath "c:\temp\file.html"
+
+
